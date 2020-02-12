dd359254041409812feb97de303a987072044c03
Sasheem <sts12@me.com>
2020-02-12 13:25:07 -0500

    added card component for clients

---
+++ src/components/Cards/cardClientFeatured.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import ButtonDonate from '../common/Button/buttonDonate';
+import ProgressBar from '../ProgressBar/progressBar';
+
+import './cards.scss';
+
+const CardClientFeatured = ({ name, situation, raised, goal }) => (
+  <div className="card-featured-container">
+    <div className="featured-image-container"></div>
+    <div className="featured-info-container">
+      <div className="featured-flex" />
+      <div className="featured-content">
+        <h4>Meet {name}</h4>
+        <p>{situation}</p>
+        <div className="featured-progress">
+          <ProgressBar percentage={(raised / goal) * 100} />
+        </div>
+        <div className="featured-status">
+          <span>
+            <p>${raised}</p>
+            <p>raised of ${goal}</p>
+          </span>
+          <ButtonDonate label="Donate" destination="/" />
+        </div>
+      </div>
+      <div className="featured-flex" />
+    </div>
+  </div>
+);
+
+export default CardClientFeatured;
