7a358028e3399fea7089f7ad5a2bc3eb9a3ca098
Sasheem <sts12@me.com>
2020-02-21 04:50:18 -0500

    firebase boiler plate

---
+++ src/components/Firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDEzuC94v5Sp8_18fy_QyUxwOVUCgAmgns',
+  authDomain: 'unhoused-website.firebaseapp.com',
+  databaseURL: 'https://unhoused-website.firebaseio.com',
+  projectId: 'unhoused-website',
+  storageBucket: 'unhoused-website.appspot.com',
+  messagingSenderId: '257547001805',
+  appId: '1:257547001805:web:60580a6aa9e2d7a53d2e84',
+  measurementId: 'G-5JVYTHGHK3',
+};
+
+export default firebaseConfig;
---
+++ src/components/Firebase/context.js
@@ -0,0 +1,8 @@
+/**
+ * * Uses react context, a global state for app
+ */
+import React from 'react';
+
+const FirebaseContext = React.createContext(null);
+
+export default FirebaseContext;
