7600470e09e5bb79ec22a7950f0f50e05a75f0d3
Sasheem <sts12@me.com>
2020-02-21 07:01:35 -0500

    implemented register user with firebase, some styling

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,18 +1,62 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 
 import { Form, Input, Button } from '../components/common';
+import { FirebaseContext } from '../components/Firebase';
 
 const Register = () => {
+  const { firebase } = useContext(FirebaseContext);
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
   function handleSubmit(event) {
     event.preventDefault();
+
+    if (formValues.password === formValues.confirmPassword) {
+      firebase.register({
+        email: formValues.email,
+        password: formValues.password,
+      });
+    }
+
+    console.dir(formValues);
+  }
+
+  function handleInputChange(event) {
+    event.persist();
+
+    setFormValues(currentValues => ({
+      ...currentValues,
+      [event.target.name]: event.target.value,
+    }));
   }
   return (
-    <Form>
-      <Input placeholder="Email" type="email" required />
-      <Input placeholder="Password" type="password" required minLength={3} />
+    <Form onSubmit={handleSubmit}>
       <Input
+        name="email"
+        placeholder="Email"
+        type="email"
+        onChange={handleInputChange}
+        value={formValues.email}
+        required
+      />
+      <Input
+        name="password"
+        placeholder="Password"
+        type="password"
+        onChange={handleInputChange}
+        value={formValues.password}
+        required
+        minLength={3}
+      />
+      <Input
+        name="confirmPassword"
         placeholder="Confirm Password"
         type="password"
+        onChange={handleInputChange}
+        value={formValues.confirmPassword}
         required
         minLength={3}
       />
