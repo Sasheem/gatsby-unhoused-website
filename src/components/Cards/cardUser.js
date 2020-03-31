8caa2019bf10bd6c4e14427aa1eb91477f59d6fa
Sasheem <sts12@me.com>
2020-03-31 15:57:48 -0400

    added icons to user info

--- src/components/Cards/cardUser.js
+++ src/components/Cards/cardUser.js
@@ -3,13 +3,16 @@ import { graphql, useStaticQuery, Link } from 'gatsby';
 import Img from 'gatsby-image';
 
 import { HorizontalDivider } from '../common';
+import WorkplaceIcon from '../../assets/user-friends-solid.svg';
+import LocationIcon from '../../assets/map-marker-alt-solid.svg';
+import MailIcon from '../../assets/envelope-solid.svg';
+import WebsiteIcon from '../../assets/link-solid.svg';
 
 import './cards.scss';
 
 /**
  * todo change query to grab all user images and
- *  todo render the one that is matched with the logged in user
- * todo add icons 4 to each bio field
+ * todo render the one that is matched with the logged in user
  * todo add edit field button to bottom right of component
  * todo add dynamic form component
  */
@@ -48,12 +51,32 @@ const CardUser = () => {
         I'm a dog dad, a star wars fan, and an avid video gamer. The homeless
         need a voice. I give back to the community when I can.
       </p>
-      <p>Front End Web Developer</p>
-      <p>Tallahassee, FL</p>
-      <p>sasheem@sasheemdev.com</p>
-      <a href="https://sasheem.dev" target="_blank">
-        sasheem.dev
-      </a>
+      <div className="card-row">
+        <div className="card-user-icon">
+          <WorkplaceIcon />
+        </div>
+        <p>Web Developer</p>
+      </div>
+      <div className="card-row">
+        <div className="card-user-icon">
+          <LocationIcon />
+        </div>
+        <p>Tallahassee, FL</p>
+      </div>
+      <div className="card-row">
+        <div className="card-user-icon">
+          <MailIcon />
+        </div>
+        <p>sasheem@sasheemdev.com</p>
+      </div>
+      <div className="card-row">
+        <div className="card-user-icon">
+          <WebsiteIcon />
+        </div>
+        <a href="https://sasheem.dev" target="_blank">
+          sasheem.dev
+        </a>
+      </div>
     </div>
   );
 };
