a97776c4b5f8d0fed5b7cf43ca5e8cad87f3709f
Sasheem <sts12@me.com>
2020-04-05 20:29:25 -0400

    updated UI styling and added more input components

--- src/pages/addClient.js
+++ src/pages/addClient.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Form, Input, Button } from '../components/common';
 import { FirebaseContext } from '../components/Firebase';
+import DatePicker from 'react-date-picker';
+
+import { HorizontalDivider } from '../components/common';
 
 import '../styles/global.scss';
 
@@ -20,13 +23,26 @@ const AddClient = () => {
     questions: [],
     answers: [],
     situation: '',
-    status: 'Unhoused',
-    dateFundingBegan: '',
-    dateHoused: '',
+    status: '',
+    question1:
+      'Where did you grow up and what did you like about your home town?',
+    question2:
+      'Do you have any brothers or sisters? If so, tell us about them.',
+    question3:
+      'Do you have any goals you would like to achieve in the short term, long term, or both?',
+    question4: '',
+    question5: '',
+    answer1: '',
+    answer2: '',
+    answer3: '',
+    answer4: '',
+    answer5: '',
   });
   const [success, setSuccess] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const [clientImage, setClientImage] = useState('');
+  const [dateFundingBegan, setDateFundingBegan] = useState(new Date());
+  const [dateHoused, setDateHoused] = useState(new Date());
   let isMounted = true;
 
   useEffect(() => {
@@ -81,86 +97,249 @@ const AddClient = () => {
     }));
   }
 
+  function handleFundingDateChange(date) {
+    setSuccess(false);
+    setDateFundingBegan(date);
+  }
+  function handleDateHousedChanged(date) {
+    setSuccess(false);
+    setDateHoused(date);
+  }
+
   return (
-    <Form onSubmit={handleSubmit}>
-      <div className="form-field">
-        <label for="firstName">First Name</label>
-        <Input
-          type="text"
-          onChange={handleInputChange}
-          value={formValues.firstName}
-          name="firstName"
-          placeholder="Client first name"
-        />
-      </div>
-      <div className="form-field">
-        <label for="lastName">Last Name</label>
-        <Input
-          type="text"
-          onChange={handleInputChange}
-          value={formValues.lastName}
-          name="lastName"
-          placeholder="Client last name"
-        />
-      </div>
-      <div className="form-field">
-        <label for="situation">Situation</label>
-        <Input
-          type="text"
-          onChange={handleInputChange}
-          value={formValues.situation}
-          name="situation"
-          placeholder="Client situation"
-        />
-      </div>
-      <div className="form-field">
-        <label for="goal">Goal</label>
-        <Input
-          onChange={handleInputChange}
-          value={formValues.goal}
-          name="goal"
-          placeholder="Amount between 10 - 1500"
-        />
-      </div>
-      <div className="form-field">
-        <label for="raised">Raised</label>
-        <Input
-          onChange={handleInputChange}
-          value={formValues.raised}
-          name="raised"
-          placeholder="Amount between 10 - 1500"
-        />
-      </div>
-      <div className="form-field">
-        <label for="familySize">Family Size</label>
-        <Input
-          onChange={handleInputChange}
-          value={formValues.familySize}
-          name="familySize"
-          placeholder="Amount between 1 - 15"
-        />
-      </div>
-      <div className="form-field">
-        <label for="clientImage">Client image</label>
-        <Input
-          onChange={e => {
-            e.persist();
-            fileReader.readAsDataURL(e.target.files[0]);
-          }}
-          type="file"
-          name="clientImage"
-        />
-      </div>
-      <Button type="submit" block>
-        Add Client
-      </Button>
-      {!!success && (
-        <div className="success-message">Client successfully created</div>
-      )}
-      {!!errorMessage && (
-        <div className="error-message">ERROR: {errorMessage}</div>
-      )}
-    </Form>
+    <div className="form-layout-admin">
+      <form onSubmit={handleSubmit} className="form-component">
+        <h3>Add a client</h3>
+        <div className="two-input-row">
+          <div className="form-input-row">
+            <label for="firstName">First Name</label>
+            <input
+              type="text"
+              onChange={handleInputChange}
+              value={formValues.firstName}
+              name="firstName"
+            />
+          </div>
+          <div className="form-input-row">
+            <label for="lastName">Last Name</label>
+            <input
+              type="text"
+              onChange={handleInputChange}
+              value={formValues.lastName}
+              name="lastName"
+            />
+          </div>
+        </div>
+        <div className="form-input-row">
+          <label for="situation">Situation</label>
+          <textarea
+            type="text"
+            onChange={handleInputChange}
+            value={formValues.situation}
+            name="situation"
+          />
+        </div>
+        <div className="three-even-input-row">
+          <div className="form-input-row">
+            <label for="goal">Goal</label>
+            <input
+              type="number"
+              onChange={handleInputChange}
+              value={formValues.goal}
+              name="goal"
+              placeholder="Amount between 10 - 1500"
+              min="10"
+              max="1500"
+            />
+          </div>
+          <div className="form-input-row">
+            <label for="raised">Raised</label>
+            <input
+              type="number"
+              onChange={handleInputChange}
+              value={formValues.raised}
+              name="raised"
+              placeholder="Amount between 10 - 1500"
+              min="10"
+              max="1500"
+            />
+          </div>
+          <div className="form-input-row">
+            <label for="familySize">Family Size</label>
+            <input
+              type="number"
+              onChange={handleInputChange}
+              value={formValues.familySize}
+              name="familySize"
+              placeholder="Amount between 1 - 12"
+              min="1"
+              max="12"
+            />
+          </div>
+        </div>
+        <div className="two-input-row">
+          <div className="form-input-row">
+            <label for="clientImage">Client image</label>
+            <input
+              onChange={e => {
+                e.persist();
+                fileReader.readAsDataURL(e.target.files[0]);
+              }}
+              type="file"
+              name="clientImage"
+            />
+          </div>
+          <div className="form-input-row">
+            <label for="status">Status</label>
+            <select id="status">
+              <option value="Unhoused" selected="selected">
+                Unhoused
+              </option>
+              <option value="Housed">Housed</option>
+            </select>
+          </div>
+        </div>
+        <div className="two-input-row">
+          <div className="form-input-row">
+            <label for="dateFundingBegan">Funding Began</label>
+            <DatePicker
+              name="dateFundingBegan"
+              onChange={handleFundingDateChange}
+              value={dateFundingBegan}
+            />
+          </div>
+          <div className="form-input-row">
+            <label for="dateHoused">Date Housed</label>
+            <DatePicker
+              name="dateHoused"
+              onChange={handleDateHousedChanged}
+              value={dateHoused}
+            />
+          </div>
+        </div>
+        <h5>
+          Enter questions/answers exactly how you want to appear on website.
+        </h5>
+        <div className="form-input-row">
+          <div className="form-input-row">
+            <label for="question1">Question 1</label>
+            <input
+              type="text"
+              name="question1"
+              onChange={handleInputChange}
+              value={formValues.question1}
+            />
+          </div>
+          <div className="form-input-row">
+            <label for="answer1">Answer 1</label>
+            <input
+              type="text"
+              name="answer1"
+              onChange={handleInputChange}
+              value={formValues.answer1}
+            />
+          </div>
+        </div>
+        <HorizontalDivider />
+        <div className="form-input-row">
+          <div className="form-input-row">
+            <label for="question2">Question 2</label>
+            <input
+              type="text"
+              name="question2"
+              onChange={handleInputChange}
+              value={formValues.question2}
+            />
+          </div>
+          <div className="form-input-row">
+            <label for="answer2">Answer 2</label>
+            <input
+              type="text"
+              name="answer2"
+              onChange={handleInputChange}
+              value={formValues.answer2}
+            />
+          </div>
+        </div>
+        <HorizontalDivider />
+        <div className="form-input-row">
+          <div className="form-input-row">
+            <label for="question3">Question 3</label>
+            <input
+              type="text"
+              name="question3"
+              onChange={handleInputChange}
+              value={formValues.question3}
+            />
+          </div>
+          <div className="form-input-row">
+            <label for="answer3">Answer 3</label>
+            <input
+              type="text"
+              name="answer3"
+              onChange={handleInputChange}
+              value={formValues.answer3}
+            />
+          </div>
+        </div>
+        <HorizontalDivider />
+        <div className="form-input-row">
+          <div className="form-input-row">
+            <label for="question4">Question 4</label>
+            <input
+              type="text"
+              name="question4"
+              onChange={handleInputChange}
+              value={formValues.question4}
+            />
+          </div>
+          <div className="form-input-row">
+            <label for="answer4">Answer 4</label>
+            <input
+              type="text"
+              name="answer4"
+              onChange={handleInputChange}
+              value={formValues.answer4}
+            />
+          </div>
+        </div>
+        <HorizontalDivider />
+        <div className="form-input-row">
+          <div className="form-input-row">
+            <label for="question5">Question 5</label>
+            <input
+              type="text"
+              name="question5"
+              onChange={handleInputChange}
+              value={formValues.question5}
+            />
+          </div>
+          <div className="form-input-row">
+            <label for="answer5">Answer 5</label>
+            <input
+              type="text"
+              name="answer5"
+              onChange={handleInputChange}
+              value={formValues.answer5}
+            />
+          </div>
+        </div>
+        <HorizontalDivider />
+        <div className="form-submit-row">
+          <div />
+          <Button type="submit" block>
+            Add Client
+          </Button>
+          <div />
+        </div>
+        {!!success && (
+          <div className="success-message">Client successfully created</div>
+        )}
+        {!!errorMessage && (
+          <div className="error-message">ERROR: {errorMessage}</div>
+        )}
+      </form>
+    </div>
   );
 };
 
